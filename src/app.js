61bb8abbcaa7a5eefef6cda58a03b13fe8ed8901
gaha20 <128089822+gaha20@users.noreply.github.com>
2023-06-12 14:53:15 +0000

    styles added

--- src/app.js
+++ src/app.js
@@ -8,22 +8,33 @@ import "./assets/img/clue.png";
 import "./assets/img/heart.png";
 import "./assets/img/spades.png";
 import "./assets/img/diamond.png";
+import { read } from "@popperjs/core";
 
 window.onload = function() {
   //write your code here
 };
 
-function randomCard() {
+/*function randomCard() {
   let cards = [
     "./../img/clue.png",
     "./../img/heart.png",
     "./../img/spades.png",
-    "./../img/diamond.png",
-    "./assets/img/rigo-baby.jpg"
+    "./../img/diamond.png"
   ];
-
   return cards[Math.floor(Math.random() * cards.length)];
+}*/
+
+function randomCard() {
+  let cards = [
+    "<p style='color:red'>♦</p>",
+    "<p style='color:red'>♥</p>",
+    "<p>♠</p>",
+    "<p>♣</p>"
+  ];
+  let cardsIndex = Math.floor(Math.random() * cards.length);
+  return cards[cardsIndex];
 }
+
 function randomNumber() {
   let cards = [
     "2",
@@ -44,23 +55,14 @@ function randomNumber() {
   return cards[Math.floor(Math.random() * cards.length)];
 }
 
-const cardCreator = `
-      <div class="card container">
-      <div class="card-header bg-transparent text-start card-type">
-        <img src="${randomCard()}">
-      </div>
-      <div class="card-body text-dark">
-        <h1
-          class="card-title position-absolute top-50 start-50 translate-middle"
-        >
-          ${randomNumber()}
-        </h1>
-      </div>
-      <div class="card-footer bg-transparent text-start card-type">
-        <img src="${randomCard()}">
-      </div>
-      </div>
-`;
+const cardType = `${randomCard()}`;
+const cardNumber = `${randomNumber()}`;
 
-const pokerElement = document.querySelector("#poker");
-pokerElement.innerHTML = cardCreator;
+const typeElement = document.querySelector(".card-type");
+typeElement.innerHTML = cardType;
+
+const type2Element = document.querySelector(".card-type2");
+type2Element.innerHTML = cardType;
+
+const numberElement = document.querySelector(".card-number");
+numberElement.innerHTML = cardNumber;
