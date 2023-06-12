3ed612e4414f7914999c5956d761959e371d7cee
gaha20 <128089822+gaha20@users.noreply.github.com>
2023-06-12 14:26:55 +0000

    images added

--- src/app.js
+++ src/app.js
@@ -4,13 +4,23 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import "./assets/img/clue.png";
+import "./assets/img/heart.png";
+import "./assets/img/spades.png";
+import "./assets/img/diamond.png";
 
 window.onload = function() {
   //write your code here
 };
 
 function randomCard() {
-  let cards = ["♦", "♥", "♠", "♣"];
+  let cards = [
+    "./../img/clue.png",
+    "./../img/heart.png",
+    "./../img/spades.png",
+    "./../img/diamond.png",
+    "./assets/img/rigo-baby.jpg"
+  ];
 
   return cards[Math.floor(Math.random() * cards.length)];
 }
@@ -37,21 +47,20 @@ function randomNumber() {
 const cardCreator = `
       <div class="card container">
       <div class="card-header bg-transparent text-start card-type">
-        Type
+        <img src="${randomCard()}">
       </div>
       <div class="card-body text-dark">
         <h1
           class="card-title position-absolute top-50 start-50 translate-middle"
         >
-          Number
+          ${randomNumber()}
         </h1>
       </div>
-      <div class="card-footer bg-transparent text-end card-type">
-        Type
+      <div class="card-footer bg-transparent text-start card-type">
+        <img src="${randomCard()}">
       </div>
       </div>
 `;
 
 const pokerElement = document.querySelector("#poker");
-
-document.getElementById(pokerElement).innerHTML = cardCreator;
+pokerElement.innerHTML = cardCreator;
